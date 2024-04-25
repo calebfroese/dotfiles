b7ede73e0418d8f4a1e3edda22daff253849a612
Caleb Froese <calebf@canva.com>
2024-04-25 23:57:49 +0930

    Update windows bat

---
+++ dot_fonts/install.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding(SupportsShouldProcess)]
+param()
+
+$fontFiles = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
+Get-ChildItem $PSScriptRoot -Filter "Fantasque:*" -Recurse | Foreach-Object {$fontFiles.Add($_)}
+
+$fonts = $null
+foreach ($fontFile in $fontFiles) {
+    $shellApp = New-Object -ComObject shell.application
+    $fonts = $shellApp.NameSpace(0x14)
+    $fonts.CopyHere($fontFile.FullName)
+}
+
