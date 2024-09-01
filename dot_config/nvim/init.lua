5e7c1d8d95fbdc1eaf5c78bc842a98045fbd2717
Caleb Froese <caleb.froese@gmail.com>
2024-09-01 20:51:10 +0930

    Improve QOL help

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,28 @@
-require "keymaps"
-require "configuration"
+require("keymaps")
+require("configuration")
 
 -- Bootstrap LazyVim
-require "config.lazy"
+require("config.lazy")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Open help in current buffer instead of horizontal split
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("HelpReplaceWindow", { clear = false }),
+  callback = function()
+    if vim.bo.filetype == "help" and vim.b.already_opened == nil then
+      vim.b.already_opened = true
+
+      -- close the original window
+      local original_win = vim.fn.win_getid(vim.fn.winnr("#"))
+      local help_win = vim.api.nvim_get_current_win()
+      if original_win ~= help_win then
+        vim.api.nvim_win_close(original_win, false)
+      end
+
+      -- put the help buffer in the buffer list
+      vim.bo.buflisted = true
+    end
+  end,
+})
