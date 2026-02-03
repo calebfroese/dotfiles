0f3ade74a4f62be63e367cf38cbdee15860eaaff
Caleb Froese <calebf@canva.com>
2026-02-03 11:18:01 +1030

    Prime terraformls

--- dot_config/nvim/lua/configuration.lua
+++ dot_config/nvim/lua/configuration.lua
@@ -8,6 +8,8 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.viminfo = "!,'1000,<50,s10,h"
 
+vim.diagnostic.config({ virtual_text = true })
+
 -- Setup copy/paste with iTerm. Requires "brew install reattach-to-user-namespace" on the MacOS host and relevant setup for iTerm
 vim.o.clipboard = "unnamedplus"
 vim.o.winborder = "rounded"
@@ -26,31 +28,3 @@ if vim.loop.os_uname().sysname == "Linux" then
     },
   }
 end
-
-vim.keymap.set("n", "<leader><leader>", "<cmd>FzfLua resume<cr>", { desc = "Resume search" })
-vim.keymap.set("n", "<leader>D", function()
-	require("fzf-lua-zoxide").open({
-		callback = function(dir)
-			vim.cmd("e " .. dir)
-		end,
-		setcwd = false,
-	})
-end, { desc = "Open Directory" })
-
-vim.keymap.set("n", "<leader>F", function()
-	require("fzf-lua-zoxide").open({
-		callback = function(_)
-			vim.cmd("e .")
-		end,
-	})
-end, { desc = "Change Directory" })
-
-vim.keymap.set("n", "<leader>s:", "<cmd>FzfLua command_history<cr>", { desc = "Command History" })
-vim.keymap.set("n", "<leader>sb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>sB", "<cmd>FzfLua lines<cr>", { desc = "Buffers Contents" })
-vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua grep_project<cr>", { desc = "Grep Project" })
-vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<cr>", { desc = "File (cwd)" })
-vim.keymap.set("n", "<leader>sr", "<cmd>FzfLua oldfiles<cr>", { desc = "File (recent)" })
-vim.keymap.set("n", "<leader>sx", "<cmd>FzfLua diagnostics_document<cr>", { desc = "Diagnostics (buffer)" })
-vim.keymap.set("n", "<leader>sX", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "Diagnostics (cwd)" })
-vim.keymap.set("n", "<leader>sl", "<cmd>FzfLua lsp_references<cr>", { desc = "References" })
--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,6 +1,3 @@
-vim.diagnostic.config({
-	virtual_text = true
-})
 vim.keymap.set("n", "<leader>d", function()
 	vim.diagnostic.open_float({ focus = true })
 end)
@@ -37,3 +34,31 @@ vim.keymap.set("n", "<C-n>", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<C-X>", ":update<CR> :source<CR>")
+
+vim.keymap.set("n", "<leader><leader>", "<cmd>FzfLua resume<cr>", { desc = "Resume search" })
+vim.keymap.set("n", "<leader>D", function()
+	require("fzf-lua-zoxide").open({
+		callback = function(dir)
+			vim.cmd("e " .. dir)
+		end,
+		setcwd = false,
+	})
+end, { desc = "Open Directory" })
+
+vim.keymap.set("n", "<leader>F", function()
+	require("fzf-lua-zoxide").open({
+		callback = function(_)
+			vim.cmd("e .")
+		end,
+	})
+end, { desc = "Change Directory" })
+
+vim.keymap.set("n", "<leader>s:", "<cmd>FzfLua command_history<cr>", { desc = "Command History" })
+vim.keymap.set("n", "<leader>sb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>sB", "<cmd>FzfLua lines<cr>", { desc = "Buffers Contents" })
+vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua grep_project<cr>", { desc = "Grep Project" })
+vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<cr>", { desc = "File (cwd)" })
+vim.keymap.set("n", "<leader>sr", "<cmd>FzfLua oldfiles<cr>", { desc = "File (recent)" })
+vim.keymap.set("n", "<leader>sx", "<cmd>FzfLua diagnostics_document<cr>", { desc = "Diagnostics (buffer)" })
+vim.keymap.set("n", "<leader>sX", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "Diagnostics (cwd)" })
+vim.keymap.set("n", "<leader>sl", "<cmd>FzfLua lsp_references<cr>", { desc = "References" })
